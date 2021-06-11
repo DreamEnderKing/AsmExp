f5fdd5c443f22d2b322aed27ad996050963e0e05
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2021-06-11 19:48:30 +0800

    Update a01.asm

--- a01.asm
+++ a01.asm
@@ -1,11 +1,18 @@
-global main
-main:
-  mov eax,4 ;　　 4号调用
-  mov ebx,1 ;　　 ebx送1表示输出
-  mov ecx,msge ;　字符串的首地址送入ecx
-  mov edx,14 ;　　字符串的长度送入edx
-  int 80h ;　　　 输出字串
-  mov eax,1 ;　　 1号调用
-  int 80h ;　　　 结束　
-msge:
-  db "Hello world!",0ah,0dh
+assume cs:code,ds:data
+
+data segment ;数据段
+    string db 'Hello,World!$'
+data ends
+
+code segment ;代码段
+  start:
+    mov ax,data ;获取段基址
+    mov ds,ax ;将段基址送入寄存器
+    mov dx,offset string
+    mov ah,9
+    int 21h
+    mov ah,4ch
+    int 21h
+code ends
+
+end start
