bba7690ae1f58a08744977232285bf9089ee8703
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2021-06-12 10:33:12 +0800

    Update a01.asm

--- a01.asm
+++ a01.asm
@@ -2,13 +2,13 @@ global _start
 
 section .text
     _start:
-        mov rax, 1                  ; sys_write的系统调用编号为1
-        mov rdi, 1                  ; 文件句柄1对应stdout
-        mov rsi, msg                ; 要输出的字符串地址
-        mov rdx, msglen             ; 要输出的字符串长度
+        mov ax, 1                  ; sys_write的系统调用编号为1
+        mov di, 1                  ; 文件句柄1对应stdout
+        mov si, msg                ; 要输出的字符串地址
+        mov dx, msglen             ; 要输出的字符串长度
         syscall                     ; 系统调用
-        mov rax, 60                 ; sys_exit的系统调用编号为60
-        xor rdi, rdi                ; exit 0
+        mov ax, 60                 ; sys_exit的系统调用编号为60
+        xor di, di                ; exit 0
         syscall
 
 section .data
