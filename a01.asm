0c3a5e41720fdce2eb2d9ae81d6b9430e129b063
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2021-06-13 15:29:24 +0800

    Update a01.asm

--- a01.asm
+++ a01.asm
@@ -2,17 +2,17 @@ global _start
 
 section .text
     _start:
-        mov eax, 1                  ; sys_write的系统调用编号为1
-        mov edi, 1                  ; 文件句柄1对应stdout
         mov esi, msg                ; 要输出的字符串地址
         mov edx, msglen             ; 要输出的字符串长度
         mov ecx, edx                ; 全部转换为大写
-        mov edi, 0
-      c:mov ebl, [cs:si+di]
-        and ebl, 11011111b
-        mov [cs:si+bi], ebl
-        add di, 1
+        mov ebx, 0
+      c:mov eal, [cs:bx+si]
+        and eal, 11011111b
+        mov [cs:bx+si], eal
+        add bx, 1
         loop c
+        mov eax, 1                  ; sys_write的系统调用编号为1
+        mov edi, 1                  ; 文件句柄1对应stdout
         syscall                     ; 系统调用
         mov eax, 60                 ; sys_exit的系统调用编号为60
         xor edi, edi                ; exit 0
