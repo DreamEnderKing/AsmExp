0ab05d5d205ade8cf8e146ed31bccaaec44e6882
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2021-06-13 15:22:31 +0800

    Update a01.asm

--- a01.asm
+++ a01.asm
@@ -6,6 +6,13 @@ section .text
         mov edi, 1                  ; 文件句柄1对应stdout
         mov esi, msg                ; 要输出的字符串地址
         mov edx, msglen             ; 要输出的字符串长度
+        mov ecx, edx                ; 全部转换为大写
+        mov edi, 0
+      c:mov ebl, [si+di]
+        and ebl, 11011111b
+        mov [si+bi], ebl
+        add di, 1
+        loop c
         syscall                     ; 系统调用
         mov eax, 60                 ; sys_exit的系统调用编号为60
         xor edi, edi                ; exit 0
